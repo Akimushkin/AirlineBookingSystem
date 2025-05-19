281194d4a579871e0412d7412d0800f1a054ae9d
Sergey Akimushkin <akimushkin@outlook.com>
2025-05-19 17:17:18 +0300

    Adding Repositories interfaces for all Microservices

---
+++ AirlineBookingSystem/AirlineBookingSystem.Bookings.Core/Repositories/IBookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using AirlineBookingSystem.Bookings.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Bookings.Core.Repositories
+{
+    public interface IBookingRepository
+    {
+        Task<Booking> GetBookingByIdAsync(int id);
+
+        Task AddBookingAsync(Booking booking);
+    }
+}
---
+++ AirlineBookingSystem/AirlineBookingSystem.Payments.Core/Repositories/IPaymantRepository.cs
@@ -0,0 +1,15 @@
+﻿using AirlineBookingSystem.Payments.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Payments.Core.Repositories
+{
+    public interface IPaymantRepository
+    {
+        Task ProcessPaymentAsunc(Paymant payment);
+        Task RefundPaymantAsync(Guid id);
+    }
+}
