4a11074ce2404d73e609396fd6a21d8322f64908
Sergey Akimushkin <akimushkin@outlook.com>
2025-05-20 18:17:35 +0300

    NotificationRepository Infrastructure implemented

---
+++ AirlineBookingSystem/AirlineBookingSystem.Notifications.Infrastructure/Repositories/NotificationRepository.cs
@@ -0,0 +1,30 @@
+﻿using AirlineBookingSystem.Notifications.Core.Entities;
+using AirlineBookingSystem.Notifications.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace AirlineBookingSystem.Notifications.Infrastructure.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly IDbConnection _dbConnection;
+        public NotificationRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task LogNotificationAsync(Notification notification)
+        {
+            const string sql = @"
+                INSERT INTO Notifications (Id, Recepient, Message, Type)
+                VALUES (@Id, @Recepient, @Message, @Type)
+            ";
+
+            await _dbConnection.ExecuteAsync(sql, notification);
+        }
+    }
