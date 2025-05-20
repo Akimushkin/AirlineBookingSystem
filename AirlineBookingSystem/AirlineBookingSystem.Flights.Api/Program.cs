a85218e52766785fec365ab4cfb0c01b5ffa5ecd
Sergey Akimushkin <akimushkin@outlook.com>
2025-05-20 15:45:21 +0300

    BookingRepository and FlightRepository in Infrastructure implemented

--- AirlineBookingSystem/AirlineBookingSystem.Flights.Api/Program.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Flights.Api/Program.cs
@@ -1,3 +1,8 @@
+using AirlineBookingSystem.Flights.Core.Repositories;
+using AirlineBookingSystem.Flights.Infrastructure.Repositories;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Application Services
+builder.Services.AddScoped<IFlightRepository, FlightRepository>();
+
+//Add Sql Connetion
+builder.Services.AddScoped<IDbConnection>(sp => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
