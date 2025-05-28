0209ff6181a60ee438928944376d0ec4c10de606
Sergey Akimushkin <akimushkin@outlook.com>
2025-05-28 14:05:29 +0300

    Notifications.Application implemented

---
+++ AirlineBookingSystem/AirlineBookingSystem.Notifications.Application/Services/NotificationService.cs
@@ -0,0 +1,22 @@
+﻿using AirlineBookingSystem.Notifications.Application.Interfaces;
+using AirlineBookingSystem.Notifications.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Notifications.Application.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public async Task SendNotificationAsync(Notification notification)
+        {
+            // Simulate sending notification (via email or SMS)
+            Console.WriteLine($"Notification sent to {notification.Recepient} : {notification.Message}" );
+
+
+        }
+    }
+}
