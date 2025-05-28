e430bb6204cd0312de5c3da16fc4987daebf37d1
Sergey Akimushkin <akimushkin@outlook.com>
2025-05-28 13:28:24 +0300

    Flights.Application implemented

---
+++ AirlineBookingSystem/AirlineBookingSystem.Flights.Application/Commands/CreateFlightCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineBookingSystem.Flights.Application.Commands
+{
+    public record CreateFlightCommand(string FlightNumber, string Origin, string Destonation, DateTime DepartureTime, DateTime ArrivalTime) : IRequest<Guid>;
+}
